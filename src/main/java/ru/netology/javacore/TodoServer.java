6b9a134d6ebd009ba44f73287206ea06bf1da6eb
budutaki <takibudu@gmail.com>
2022-07-01 16:50:43 +0300

    Code was ToDo Server added

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,21 +1,64 @@
 package ru.netology.javacore;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class TodoServer {
-    //...
+    protected int port;
+    protected Todos todos;
 
     public TodoServer(int port, Todos todos) {
-        //...
+        this.port = port;
+        this.todos = todos;
     }
 
-    public void start() throws IOException {
+    public Task jsonToData(String json) throws ParseException {
+        JSONParser jp = new JSONParser();
+        GsonBuilder gb = new GsonBuilder();
+        Gson gson = gb.create();
+        JSONObject jsonObject = (JSONObject) jp.parse(json);
+        return gson.fromJson(String.valueOf(jsonObject), Task.class);
+    }
+
+
+    public void start() {
         System.out.println("Starting server at " + port + "...");
-        //...
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream())
+                ) {
+                    final String json = in.readLine();
+                    final Task result = jsonToData(json);
+                    switch (result.getType()) {
+                        case ADD:
+                            todos.addTask(result.getTask());
+                            break;
+                        case REMOVE:
+                            todos.removeTask(result.getTask());
+                            break;
+                        case LIST:
+                            todos.getAllTasks();
+                            break;
+
+                    }
+                    out.println(todos.getAllTasks());
+                }
+            }
+        } catch (IOException | ParseException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
+
+
     }
 }
--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -1,21 +1,59 @@
 package ru.netology.javacore;
-
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Todos {
-    //...
+public List<String> tasks;
+
+    public Todos() {
+        this.tasks = new ArrayList<>() {
+        };
+    }
 
     public void addTask(String task) {
-        //...
+        tasks.add(task + " ");
     }
 
     public void removeTask(String task) {
-        //...
+        tasks.remove(task);
     }
 
     public String getAllTasks() {
-        //...
+        StringBuilder sb = new StringBuilder();
+        Stream <String> ts = tasks.stream()
+                .sorted(Comparator.naturalOrder());
+                tasks = ts.collect(Collectors.toList());
+        for (String task : tasks) {
+            sb.append(task + " ");
+        }
+        return String.valueOf(sb);
+    }
     }
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
